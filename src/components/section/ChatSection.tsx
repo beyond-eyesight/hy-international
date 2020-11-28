3392a5f86699f7db52b299d8de285a30fc298fea
junk-sound <wom2277@naver.com>
2020-11-28 22:02:04 +0900

    refac: onSend

--- src/components/section/ChatSection.tsx
+++ src/components/section/ChatSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
@@ -27,24 +27,30 @@ const ChatSection: React.FC = () => {
     'accept-version': '1.2'
   };
 
-  function send(newMessages: IMessage[]) {
-    ws.connect(headers, () => {
-      ws.subscribe(
-        `/sub/chat/room/11084123-e6fb-4191-8fd8-5674a5107c33`,
-        () => {},
-        {
-          id: '1',
-          ack: 'client'
-        }
-      );
-    });
-    setMessages(GiftedChat.append(messages, newMessages));
-  }
+  // function send(newMessages: IMessage[]) {
+  //   ws.connect(headers, () => {
+  //     ws.subscribe(
+  //       `/sub/chat/room/11084123-e6fb-4191-8fd8-5674a5107c33`,
+  //       () => {},
+  //       {
+  //         id: '1',
+  //         ack: 'client'
+  //       }
+  //     );
+  //   });
+  //   setMessages(GiftedChat.append(messages, newMessages));
+  // }
+
+  const onSend = useCallback((newMessages: IMessage[] = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  }, []);
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={(messages: IMessage[]) => send(messages)}
+      onSend={(messages: IMessage[]) => onSend(messages)}
       user={{
         _id: 1
       }}
