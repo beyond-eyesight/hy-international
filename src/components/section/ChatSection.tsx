abfb27bd25ead9399f7500594f84697e6c918a83
junk-sound <wom2277@naver.com>
2020-11-30 16:57:50 +0900

    feat: 일단 send, subscribe 되게 함

--- src/components/section/ChatSection.tsx
+++ src/components/section/ChatSection.tsx
@@ -1,58 +1,103 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { GiftedChat, IMessage } from 'react-native-gifted-chat';
-import { Stomp } from '@stomp/stompjs';
+import { CompatClient, Message, Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import 'text-encoding-polyfill';
+import { User } from 'react-native-gifted-chat/lib/Models';
+import { StompHeaders } from '@stomp/stompjs/esm6/stomp-headers';
+
+class MessageType implements IMessage {
+  _id: string | number;
+
+  text: string;
+
+  createdAt: Date | number;
+
+  user: User;
+
+  constructor(
+    id: string | number,
+    text: string,
+    createdAt: Date | number,
+    user: User
+  ) {
+    this._id = id;
+    this.text = text;
+    this.createdAt = createdAt;
+    this.user = user;
+  }
+}
+
+interface ISubscribe {
+  (
+    destination: string,
+    callback: (message: Message) => void,
+    header?: StompHeaders
+  ): void;
+}
 
 // todo: refac
 const ChatSection: React.FC = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 3,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
-      }
-    ]);
-  }, []);
-
   const ws = Stomp.over(() => new SockJS('http://localhost:8080/ws-stomp'));
-  const headers = {
-    'accept-version': '1.2'
-  };
 
-  // function send(newMessages: IMessage[]) {
-  //   ws.connect(headers, () => {
-  //     ws.subscribe(
-  //       `/sub/chat/room/11084123-e6fb-4191-8fd8-5674a5107c33`,
-  //       () => {},
-  //       {
-  //         id: '1',
-  //         ack: 'client'
-  //       }
-  //     );
-  //   });
-  //   setMessages(GiftedChat.append(messages, newMessages));
-  // }
-
-  const onSend = useCallback((newMessages: IMessage[] = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
+  useEffect(() => {
+    ws.connect(
+      {
+        'accept-version': '1.2'
+      },
+      () => {
+        ws.subscribe(
+          `/sub/chat/room/110841e3-e6fb-4191-8fd8-5674a5107c33`,
+          (message: Message) => {
+            const body = JSON.parse(message.body);
+            setMessages(
+              GiftedChat.append(messages, [
+                new MessageType(body.id, body.body, new Date(), {
+                  _id: body.sender,
+                  name: 'Juhyun',
+                  avatar: 'https://placeimg.com/140/140/any'
+                })
+              ])
+            );
+          },
+          {
+            id: '1',
+            ack: 'client'
+          }
+        );
+      }
     );
-  }, []);
+  }, [ws, messages]);
+  const onSend = useCallback(
+    (newMessages: IMessage[] = []) => {
+      const body =
+        '{' +
+        '"id": "110841e3-e6fb-4191-8fd8-5674a5107c33",' +
+        '"chatRoomId": "110841e3-e6fb-4191-8fd8-5674a5107c33",' +
+        '"sender": "110841e3-e6fb-4191-8fd8-5674a5107c33",' +
+        '"body": "kiki"' +
+        '}';
+      ws.send(
+        `/sub/chat/room/110841e3-e6fb-4191-8fd8-5674a5107c33`,
+        {
+          'content-type': 'application/json'
+        },
+        body
+      );
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, newMessages)
+      );
+    },
+    [ws]
+  );
 
   return (
     <GiftedChat
       messages={messages}
       onSend={(messages: IMessage[]) => onSend(messages)}
       user={{
-        _id: 1
+        _id: 4
       }}
     />
   );
