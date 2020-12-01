10271884dd0e169997ab22dc9c4afa8c35f019e1
junk-sound <wom2277@naver.com>
2020-12-01 13:41:18 +0900

    refac: ChatMessageDto

--- src/components/section/ChatSection.tsx
+++ src/components/section/ChatSection.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-classes-per-file
 import React, { useEffect, useState } from 'react';
 import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 import { CompatClient, Message, Stomp } from '@stomp/stompjs';
@@ -31,7 +32,7 @@ const ChatSection: React.FC = () => {
 };
 
 class ChatMessage implements IMessage {
-  _id: string | number;
+  _id: string;
 
   text: string;
 
@@ -39,12 +40,7 @@ class ChatMessage implements IMessage {
 
   user: User;
 
-  constructor(
-    id: string | number,
-    text: string,
-    createdAt: Date | number,
-    user: User
-  ) {
+  constructor(id: string, text: string, createdAt: Date | number, user: User) {
     this._id = id;
     this.text = text;
     this.createdAt = createdAt;
@@ -52,6 +48,31 @@ class ChatMessage implements IMessage {
   }
 }
 
+class ChatMessageDto {
+  constructor(id: string, chatRoomId: string, senderId: string, body: string) {
+    this.id = id;
+    this.chatRoomId = chatRoomId;
+    this.senderId = senderId;
+    this.body = body;
+  }
+
+  id: string;
+
+  chatRoomId: string;
+
+  senderId: string;
+
+  body: string;
+
+  static fromIMessage(message: IMessage) {
+    return new ChatMessageDto(
+      message._id as string,
+      '110841e3-e6fb-4191-8fd8-5674a5107c33',
+      message.user._id as string,
+      message.text
+    );
+  }
+}
 function onConnect(
   ws: CompatClient,
   setMessages: (
@@ -64,11 +85,11 @@ function onConnect(
     ws.subscribe(
       `/sub/chat/room/110841e3-e6fb-4191-8fd8-5674a5107c33`,
       (message: Message) => {
-        const body = JSON.parse(message.body);
+        const body: ChatMessageDto = JSON.parse(message.body);
         setMessages(
           GiftedChat.append(messages, [
             new ChatMessage(body.id, body.body, new Date(), {
-              _id: body.sender,
+              _id: body.senderId,
               name: 'Juhyun',
               avatar: 'https://placeimg.com/140/140/any'
             })
@@ -90,20 +111,17 @@ function onSend(
   ) => void,
   newMessages: IMessage[] = []
 ) {
-  const body =
-    '{' +
-    '"id": "110841e3-e6fb-4191-8fd8-5674a5107c33",' +
-    '"chatRoomId": "110841e3-e6fb-4191-8fd8-5674a5107c33",' +
-    '"sender": "110841e3-e6fb-4191-8fd8-5674a5107c33",' +
-    '"body": "kiki"' +
-    '}';
-  ws.send(
-    `/sub/chat/room/110841e3-e6fb-4191-8fd8-5674a5107c33`,
-    {
-      'content-type': 'application/json'
-    },
-    body
-  );
+  // todo: 여러개의 메시지를 한번에 SEND할 수 있을까?
+  newMessages.forEach((newMessage) => {
+    const chatMessageDto = ChatMessageDto.fromIMessage(newMessage);
+    ws.send(
+      `/sub/chat/room/110841e3-e6fb-4191-8fd8-5674a5107c33`,
+      {
+        'content-type': 'application/json'
+      },
+      JSON.stringify(chatMessageDto)
+    );
+  });
   setMessages((previousMessages) =>
     GiftedChat.append(previousMessages, newMessages)
   );
