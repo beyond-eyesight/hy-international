cf1efbb7f89e71dc9a56b80d4c792d90ed5c10d7
junk-sound <wom2277@naver.com>
2020-12-06 13:44:55 +0900

    refac: 절대경로?

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,18 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['babel-plugin-styled-components'],
-    ['@babel/plugin-proposal-decorators', { legacy: true }]
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '~/*': './src',
+          test: './test',
+          underscore: 'lodash'
+        },
+        cwd: 'babelrc'
+      }
+    ]
   ]
 };
