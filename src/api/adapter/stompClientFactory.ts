730b8920841bd82f3a627af5bb94376dfb0b1c6c
junk-sound <wom2277@naver.com>
2020-12-09 14:57:29 +0900

    chore: reference 주석

--- src/api/adapter/stompClientFactory.ts
+++ src/api/adapter/stompClientFactory.ts
@@ -14,6 +14,10 @@ const createStompClient = (
   if (webSocketVersion === WebSocketVersion.REACT_NATIVE) {
     return Stomp.over(() => new WebSocket(url));
   }
+
+  // todo: appendingMissingNullonIncoming: true 는 메시지의 크기가 커지면 문제가 될 수 있다고 한다. 현재는 리액티 네이티브에서 이 옵션이 필요한데,
+  //  나중에 리액트 네이티브가 업데이트되면 이 옵션이 필요 없어질 수 있다. 그 때 지우기!
+  //  reference: https://stomp-js.github.io/workaround/stompjs/rx-stomp/ng2-stompjs/react-native-additional-notes.html
   return new Client({
     brokerURL: 'ws://localhost:8080/ws-stomp',
     reconnectDelay: 0,
