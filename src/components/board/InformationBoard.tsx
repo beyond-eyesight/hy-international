3e6cb0f21fcf469b2b38bc8789b6ed4631c43486
junk-sound <wom2277@naver.com>
2021-03-11 10:25:35 +0900

    title board

--- src/components/board/InformationBoard.tsx
+++ src/components/board/InformationBoard.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode } from 'react';
-import { View } from 'react-native';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
 import TextBox from 'src/components/box/TextBox';
 import { TextInputBoxStyle } from 'src/components/box/TextInputBox';
 
 interface Props {
   title: string;
-  children: ReactNode;
+  children?: ReactNode;
   titleStyles: TextInputBoxStyle;
   bodyStyles: TextInputBoxStyle;
 }
@@ -16,6 +16,7 @@ const InformationBoard: React.FC<Props> = ({
   titleStyles,
   bodyStyles
 }: Props) => {
+  console.log(children);
   return (
     <View>
       <TextBox
@@ -24,12 +25,14 @@ const InformationBoard: React.FC<Props> = ({
       >
         {title}
       </TextBox>
-      <TextBox
-        boxStyle={bodyStyles.boxStyle}
-        textStyle={bodyStyles.contentStyle}
-      >
-        {children}
-      </TextBox>
+      {children !== undefined && (
+        <TextBox
+          boxStyle={bodyStyles.boxStyle}
+          textStyle={bodyStyles.contentStyle}
+        >
+          {children}
+        </TextBox>
+      )}
     </View>
   );
 };
--- src/components/section/ZoneSection.tsx
+++ src/components/section/ZoneSection.tsx
@@ -2,22 +2,57 @@ import React, { useState } from 'react';
 import Zone from 'src/model/zone';
 import Pixel from 'src/draw/size/pixel';
 import Percentage from 'src/draw/size/percentage';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import runningDeviceModel from '../../draw/device/model/deviceModel';
+import InformationBoard from '../board/InformationBoard';
+import { TextInputBoxStyle } from '../box/TextInputBox';
 
 export type Props = {
   componentId: string;
 };
 
 const deviceModelHeight: Pixel = runningDeviceModel._height;
+const deviceModelWidth: Pixel = runningDeviceModel._width;
 
 const textSize: Pixel = deviceModelHeight.multiply(new Percentage(2.5));
 
 const ZoneSection: React.FC<Props> = ({ componentId }: Props) => {
   const [chatRooms, setChatRooms] = useState<Zone[]>();
+
   return (
     <View>
+      <TitleBoard />
     </View>
+  );
+};
+
+const TitleBoard: React.FC = () => {
+  const titleStyles = StyleSheet.create<TextInputBoxStyle>({
+    boxStyle: {
+      width: deviceModelWidth.multiply(new Percentage(90)).value,
+      height: deviceModelHeight.multiply(new Percentage(4)).value,
+      alignItems: 'center',
+      alignSelf: 'center',
+      justifyContent: 'center',
+      marginTop: deviceModelHeight.multiply(new Percentage(5)).value
+    },
+    contentStyle: {
+      fontFamily: 'ProximaNova-Bold',
+      fontSize: deviceModelHeight.multiply(new Percentage(4)).value,
+      lineHeight: deviceModelHeight.multiply(new Percentage(4)).value,
+      color: 'black'
+    }
+  });
+
+  return (
+    <InformationBoard
+      title="Endter the zone"
+      titleStyles={titleStyles}
+      bodyStyles={StyleSheet.create<TextInputBoxStyle>({
+        boxStyle: {},
+        contentStyle: {}
+      })}
+    />
   );
 };
 
