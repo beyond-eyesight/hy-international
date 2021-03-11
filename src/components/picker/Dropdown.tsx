297cb25dca1018bd47bedd4c72bcbdb83dd2bf26
junk-sound <wom2277@naver.com>
2021-03-11 13:28:45 +0900

    remove trash

--- src/components/picker/Dropdown.tsx
+++
@@ -1,9 +0,0 @@
-import { View, ViewStyle } from 'react-native';
-import React from 'react';
-import DropDownPicker from 'react-native-dropdown-picker';
-
-export interface DropdownPickerStyleProps extends DropDownPicker {}
-
-const DropdownPicker: React.FC<DropdownPickerStyleProps> = () => {
-  return <View />;
-};
