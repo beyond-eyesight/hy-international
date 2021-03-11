409dc7fd3a122c7c8dd038a1a6e4034b81de0e05
junk-sound <wom2277@naver.com>
2021-03-11 11:51:17 +0900

    add count to zone

--- src/model/zone.ts
+++ src/model/zone.ts
@@ -1,10 +1,17 @@
 import ZoneId from 'src/model/zoneId';
 import ZoneName from 'src/model/zoneName';
+import Count from './count';
 
 export default class Zone {
   private readonly _id: ZoneId;
 
   private readonly _name: ZoneName;
+
+  private readonly _count: Count;
+
+  get count(): Count {
+    return this._count;
+  }
 
   get id(): ZoneId {
     return this._id;
@@ -14,12 +21,13 @@ export default class Zone {
     return this._name;
   }
 
-  static of(id: string, name: string) {
-    return new Zone(new ZoneId(id), new ZoneName(name));
+  static of(id: string, name: string, count: number) {
+    return new Zone(new ZoneId(id), new ZoneName(name), new Count(count));
   }
 
-  constructor(id: ZoneId, name: ZoneName) {
+  constructor(id: ZoneId, name: ZoneName, count: Count) {
     this._id = id;
     this._name = name;
+    this._count = count;
   }
 }
